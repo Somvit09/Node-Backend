811f72f5b4a971eb9422dec48d6dcdfd8e231f42
Teja <103510023+NaveentejaB@users.noreply.github.com>
2023-11-23 17:31:40 +0530

    updated customer.js

--- pages/customer/customer.js
+++ pages/customer/customer.js
@@ -1,25 +1,58 @@
 const Customer = require("../../models/customer_model")
+const fs = require("fs")
+const path = require("path")
+
 
 const tryVTR = async(req,res)=>{
     try{
-        const id = req.params.customerID
-        // adding dummy image from unsplash
-        const image =await axios.get("https://api.unsplash.com/photos/random/?client_id=cOoy9hV3Nu0z7dKQpa8fssqZZAtvRsDc-RKaMmwmSD8")
-        const data = image.data.urls.full
-        // finding customer with given id
-        const customer =await Customer.findOneAndUpdate({customerID:id}) 
-        const arrImgs = customer.customerVirturalTryRoomImages
-        arrImgs.push({
-           url:data
-        })
-        // update the output image array in the customer model
-        const updateCustomer = await Customer.findOneAndUpdate({customerID:id},{customerVirturalTryRoomImages:arrImgs})
-        
-        res.status(200).json({
-            imageUrl : data, // sending the image url as response
-            message:"New image updated",
-            success:true
-        })
+        const uploadFolderPath = '../../uploads/'
+        fs.readdir(uploadFolderPath,async(err,files)=> {
+            if (err) {
+              console.error('Error reading folder:', err)
+              return res.status(500).json({
+                message:err,
+                success:false
+              })
+            }
+            // const savedImages = []
+            
+            let mostRecentImage = null
+            let mostRecentTimestamp = 0
+
+            files.forEach(file => {
+            const filePath = path.join(uploadFolderPath, file)
+            const fileStats = fs.statSync(filePath)
+
+            if (fileStats.mtimeMs > mostRecentTimestamp) {
+                mostRecentTimestamp = fileStats.mtimeMs
+                mostRecentImage = file
+            }
+            })
+            if (!mostRecentImage) {
+                return res.status(404).json({
+                    message:"no image found",
+                    success:false
+                })
+            }
+
+            const customer = await Customer.findOne({customerID:req.user})
+            const images = customer.customerVirturalTryRoomImages //existing images
+            
+            // images.forEach(function(currentImage, Index){
+            //     savedImages[Index] += currentImage
+            // })
+            
+            images.push(mostRecentImage)
+
+            const updateCustomer =await Customer.findOneAndUpdate({customerID:req.user},
+                {customerVirturalTryRoomImages:images})
+
+            res.status(200).json({
+                images: images,
+                message:"images updated",
+                success:true
+            })
+          })
     }catch(err){
         res.status(500).json({
             message:err.message,
@@ -30,7 +63,7 @@ const tryVTR = async(req,res)=>{
 
 const getAllVTRImages = async(req,res)=>{
     try{
-        const customer =await Customer.findOne({customerID:req.params.customerID})
+        const customer =await Customer.findOne({customerID:req.user})
         const images = customer.customerVirturalTryRoomImages
         
         res.status(200).json({
